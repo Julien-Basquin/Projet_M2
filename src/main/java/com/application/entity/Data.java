e0b92acda083c74f152a6cc824b179ba527a94a5
Julien B <julien.basquin@hotmail.com>
2020-01-13 18:29:16 +0100

    Ajout des champs manquants dans la table Data

--- src/main/java/com/application/entity/Data.java
+++ src/main/java/com/application/entity/Data.java
@@ -14,6 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.application.entity.enumeration.DocumentEnum;
+import com.application.entity.enumeration.TypeEnum;
 
 @Entity 
 @Table(name = "DATA")
@@ -24,14 +25,39 @@ public class Data {
 	@Column(name = "ID", nullable = false)
 	private Long id;
 	
+	@Column(name = "TYPE_BIBLIOGRAPHY", nullable = false)
+	private TypeEnum typeBibliography;
+	
+	@Column(name = "NAME_BIBLIOGRAPHY", nullable = false)
+	private String nameBibliography;
+	
 	@Column(name = "ABSTRACT", length = 4096)
 	private String summary;
+	
+	@Column(name = "ADDRESS")
+	private String address;
+
+	@Column(name = "AFFILIATION")
+	private String affiliation;
+	
+	@Column(name = "ARTICLE_NUMBER")
+	private String articleNumber;
 	
 	@Column(name = "AUTHOR")
 	private String author;
 	
+	@Column(name = "AUTHOR_EMAIL")
+	private String authorEmail;
+	
 	@Column(name = "AUTHOR_KEYWORDS")
 	private String authorKeywords;
+
+	@Column(name = "BDSK_URL")
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, targetEntity = Bdsk_url.class)
+	private List<String> bdskUrl;
+	
+	@Column(name = "BOOK_TITLE")
+	private String bookTitle;
 	
 	@Column(name = "DATE_ADDED", nullable = false)
 	private Date dateAdded;
@@ -39,11 +65,41 @@ public class Data {
 	@Column(name = "DATE_MODIFIED")
 	private Date dateModified;
 	
+	@Column(name = "DOC_DELIVERY_NUMBER")
+	private String docDeliveryNumber;
+	
 	@Column(name = "DOCUMENT_TYPE", nullable = false)
 	private DocumentEnum documentType;
 	
+	@Column(name = "DOI")
+	private String doi;
+	
+	@Column(name = "EISSN")
+	private String eissn;
+	
+	@Column(name = "ISSN")
+	private String issn;
+	
 	@Column(name = "JOURNAL")
 	private String journal;
+	
+	@Column(name = "JOURNAL_ISO")
+	private String journalIso;
+	
+	@Column(name = "KEYWORDS")
+	private String keywords;
+	
+	@Column(name = "KEYWORDS_PLUS")
+	private String keywordsPlus;
+	
+	@Column(name = "LANGUAGE")
+	private String language;
+	
+	@Column(name = "MARKEDENTRY")
+	private String markedentry;
+	
+	@Column(name = "MONTH")
+	private String month;
 	
 	@Column(name = "NOTE")
 	private String note;
@@ -51,24 +107,62 @@ public class Data {
 	@Column(name = "NUMBER")
 	private int number;
 	
+	@Column(name = "NUMBER_OF_CITED_REFERENCES")
+	private int numberOfCitedReferences;
+	
+	@Column(name = "ORCID_NUMBERS")
+	private String orcidNumbers;
+	
 	@Column(name = "PAGE_BEGIN")
 	private int pageBegin;
 	
 	@Column(name = "PAGE_END")
 	private int pageEnd;
 	
+	@Column(name = "PUBLISHER")
+	private String publisher;
+	
+	@Column(name = "RESEARCH_AREAS")
+	private String researchAreas;
+	
+	@Column(name = "RESEARCH_ID_NUMBERS")
+	private String researchIdNumbers;
+	
 	@Column(name = "SOURCE")
 	private String source;
+	
+	@Column(name = "TIME_CITED")
+	private int timeCited;
 	
 	@Column(name = "TITLE")
 	private String title;
 	
+	@Column(name = "TO_DOWNLOAD")
+	private boolean toDownload;
+	
+	@Column(name = "TYPE")
+	private String type;
+	
+	@Column(name = "UNIQUE_ID")
+	private String uniqueId;
+	
 	@Column(name = "URL")
 	private String url;
+
+	@Column(name = "USAGE_COUNT_LAST_180_DAYS")
+	private int usageCountLast180Days;
 	
-	@Column(name = "BDSK_URL")
-	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, targetEntity = Bdsk_url.class)
-	private List<String> bdskUrl;
+	@Column(name = "USAGE_COUNT_SINCE_2013")
+	private int usageCountSince_2013;
+	
+	@Column(name = "VOLUME")
+	private String volume;
+	
+	@Column(name = "WEB_OF_SCIENCE_CATEGORIES")
+	private String webOfScienceCategories;
+	
+	@Column(name = "YEAR")
+	private int year;
 
 	public Data(String summary, String author, String authorKeywords, Date dateAdded, Date dateModified,
 			DocumentEnum documentType, String journal, String note, int number, int pageBegin, int pageEnd,
---
+++ src/main/java/com/application/entity/enumeration/TypeEnum.java
@@ -0,0 +1,7 @@
+package com.application.entity.enumeration;
+
+public enum TypeEnum {
+	ARTICLE,
+	CONFERENCE,
+	INPROCEEDINGS
+}
